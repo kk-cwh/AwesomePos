9ca583ed5d319d3856dd2c0db6c4ac522cc30c18
zyk <zhangykvip@126.com>
2017-07-25 17:50:53 +0800

    change to static data

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -123,76 +123,7 @@ export default {
   data: function () {
     return {
       tableData: [],
-      oftenGoods: [
-    [
-        {
-            "goodsId": 1,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-            "goodsName": "香辣鸡腿堡",
-            "price": 18
-        },
-        {
-            "goodsId": 2,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-            "goodsName": "田园鸡腿堡",
-            "price": 15
-        },
-        {
-            "goodsId": 3,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-            "goodsName": "和风汉堡",
-            "price": 15
-        }
-    ],
-    [
-        {
-            "goodsId": 4,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-            "goodsName": "大包薯条",
-            "price": 18
-        },
-        {
-            "goodsId": 5,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-            "goodsName": "脆皮炸鸡腿",
-            "price": 20
-        },
-        {
-            "goodsId": 6,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-            "goodsName": "魔法鸡块",
-            "price": 20
-        }
-    ],
-    [
-        {
-            "goodsId": 7,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-            "goodsName": "可乐大杯",
-            "price": 10
-        },
-        {
-            "goodsId": 8,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-            "goodsName": "雪顶咖啡",
-            "price": 18
-        }
-    ],
-    [
-        {
-            "goodsId": 9,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
-            "goodsName": "儿童欢乐套餐",
-            "price": 25
-        },
-        {
-            "goodsId": 10,
-            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-            "goodsName": "快乐全家桶",
-            "price": 99
-        }
-    ]
-],
+      oftenGoods: [],
       type0Goods: [
         {
             "goodsId": 1,
@@ -268,6 +199,74 @@ export default {
     //   .then(response => {
     //     this.oftenGoods = response.data;
     //   })
+    this.oftenGoods = [[
+        {
+            "goodsId": 1,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+            "goodsName": "香辣鸡腿堡",
+            "price": 18
+        },
+        {
+            "goodsId": 2,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+            "goodsName": "田园鸡腿堡",
+            "price": 15
+        },
+        {
+            "goodsId": 3,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+            "goodsName": "和风汉堡",
+            "price": 15
+        }
+    ],
+    [
+        {
+            "goodsId": 4,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+            "goodsName": "大包薯条",
+            "price": 18
+        },
+        {
+            "goodsId": 5,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+            "goodsName": "脆皮炸鸡腿",
+            "price": 20
+        },
+        {
+            "goodsId": 6,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+            "goodsName": "魔法鸡块",
+            "price": 20
+        }
+    ],
+    [
+        {
+            "goodsId": 7,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+            "goodsName": "可乐大杯",
+            "price": 10
+        },
+        {
+            "goodsId": 8,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+            "goodsName": "雪顶咖啡",
+            "price": 18
+        }
+    ],
+    [
+        {
+            "goodsId": 9,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+            "goodsName": "儿童欢乐套餐",
+            "price": 25
+        },
+        {
+            "goodsId": 10,
+            "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+            "goodsName": "快乐全家桶",
+            "price": 99
+        }
+    ]];
     // axios.get('http://jspang.com/DemoApi/typeGoods.php')
     //   .then(response => {
     //     this.type0Goods = response.data[0];
